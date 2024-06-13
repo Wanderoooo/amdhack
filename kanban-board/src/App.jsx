d1f8b3ceb28480ebf067d709b9be423c24353633
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2024-06-12 19:02:03 -0700

    removed cache

---
+++ kanban-board/src/App.jsx
@@ -0,0 +1,188 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "../components/Navbar/Navbar";
+import Board from "../components/Board/Board";
+// import data from '../data'
+import { DragDropContext } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from "uuid";
+import Editable from "../components/Editable/Editable";
+import useLocalStorage from "use-local-storage";
+import "../bootstrap.css";
+import LeaderBoard from "./Components/LeaderBoard/LeaderBoard";
+import Wave from 'react-wavify'
+import logo from './vulsquash2.png'
+
+function App() {
+  const [data, setData] = useState(
+    localStorage.getItem("kanban-board")
+      ? JSON.parse(localStorage.getItem("kanban-board"))
+      : []
+  );
+
+  const defaultDark = window.matchMedia(
+    "(prefers-colors-scheme: dark)"
+  ).matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+
+  const switchTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const setName = (title, bid) => {
+    const index = data.findIndex((item) => item.id === bid);
+    const tempData = [...data];
+    tempData[index].boardName = title;
+    setData(tempData);
+  };
+
+  const dragCardInBoard = (source, destination) => {
+    let tempData = [...data];
+    const destinationBoardIdx = tempData.findIndex(
+      (item) => item.id.toString() === destination.droppableId
+    );
+    const sourceBoardIdx = tempData.findIndex(
+      (item) => item.id.toString() === source.droppableId
+    );
+    tempData[destinationBoardIdx].card.splice(
+      destination.index,
+      0,
+      tempData[sourceBoardIdx].card[source.index]
+    );
+    tempData[sourceBoardIdx].card.splice(source.index, 1);
+
+    return tempData;
+  };
+
+  const addCard = (title, bid) => {
+    const index = data.findIndex((item) => item.id === bid);
+    const tempData = [...data];
+    tempData[index].card.push({
+      id: uuidv4(),
+      title: title,
+      tags: [],
+      task: [],
+    });
+    setData(tempData);
+  };
+
+  const removeCard = (boardId, cardId) => {
+    const index = data.findIndex((item) => item.id === boardId);
+    const tempData = [...data];
+    const cardIndex = data[index].card.findIndex((item) => item.id === cardId);
+
+    tempData[index].card.splice(cardIndex, 1);
+    setData(tempData);
+  };
+
+  const addBoard = (title) => {
+    const tempData = [...data];
+    tempData.push({
+      id: uuidv4(),
+      boardName: title,
+      card: [],
+    });
+    setData(tempData);
+  };
+
+  const removeBoard = (bid) => {
+    const tempData = [...data];
+    const index = data.findIndex((item) => item.id === bid);
+    tempData.splice(index, 1);
+    setData(tempData);
+  };
+
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+    if (!destination) return;
+
+    if (source.droppableId === destination.droppableId) return;
+
+    setData(dragCardInBoard(source, destination));
+  };
+
+  const updateCard = (bid, cid, card) => {
+    const index = data.findIndex((item) => item.id === bid);
+    if (index < 0) return;
+
+    const tempBoards = [...data];
+    const cards = tempBoards[index].card;
+
+    const cardIndex = cards.findIndex((item) => item.id === cid);
+    if (cardIndex < 0) return;
+
+    tempBoards[index].card[cardIndex] = card;
+    console.log(tempBoards);
+    setData(tempBoards);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("kanban-board", JSON.stringify(data));
+  }, [data]);
+
+  return (
+    <div>
+    <div style={{ backgroundColor: '#212121', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <Wave mask="url(#mask)" fill="#212121" >
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="0" stopColor="black" />
+            <stop offset="0.5" stopColor="white" />
+          </linearGradient>
+          <mask id="mask">
+            <rect x="0" y="0" width="2000" height="300" fill="url(#gradient)" />
+          </mask>
+        </defs>
+      </Wave>
+    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', height: '150px', marginTop: '50px' }}>
+        <img  className="name" src={logo} style={{ width: '800px', height: '500px' }} />
+      </div>
+    <Wave mask="url(#mask)" fill="#212121" >
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="0" stopColor="black" />
+            <stop offset="0.5" stopColor="white" />
+          </linearGradient>
+          <mask id="mask">
+            <rect x="0" y="0" width="2000" height="300" fill="url(#gradient)" />
+          </mask>
+        </defs>
+      </Wave>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App" data-theme={theme}>
+        <Navbar switchTheme={switchTheme} />
+        <div className="app_outer">
+          <div className="app_boards">
+            {data.map((item) => (
+              <Board
+                key={item.id}
+                id={item.id}
+                name={item.boardName}
+                card={item.card}
+                setName={setName}
+                addCard={addCard}
+                removeCard={removeCard}
+                removeBoard={removeBoard}
+                updateCard={updateCard}
+              />
+            ))}
+            <Editable
+              class={"add__board"}
+              name={"Add Board"}
+              btnName={"Add Board"}
+              onSubmit={addBoard}
+              placeholder={"Enter Board  Title"}
+            />
+          </div>
+        </div>
+      </div>
+    </DragDropContext>
+    </div>
+    <LeaderBoard/>
+    </div>
+  );
+}
+
+export default App;
